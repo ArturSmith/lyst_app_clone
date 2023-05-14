0d54412d730442787454a7690dfe073c44b6095b
Artur Smith <arthur9smith7@gmail.com>
2023-05-14 14:42:20 +0200

    Created Product class.

--- app/src/main/java/com/example/foodi/Product.java
+++ app/src/main/java/com/example/foodi/Product.java
@@ -1,9 +1,71 @@
 package com.example.foodi;
 
+import androidx.room.PrimaryKey;
+
+import com.google.gson.annotations.SerializedName;
+
+
 public class Product {
-    private String id;
+
+    @PrimaryKey
+    @SerializedName("id")
+    private int id;
+    @SerializedName("image")
     private String image;
+    @SerializedName("price")
     private String price;
-    private String brand;
+    @SerializedName("title")
+    private String title;
+    @SerializedName("description")
     private String description;
+    @SerializedName("category")
+    private String category;
+    @SerializedName("rating")
+    private Rating rating;
+
+    public Product(int id, String image, String price, String title, String description, String category, Rating rating) {
+        this.id = id;
+        this.image = image;
+        this.price = price;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", image='" + image + '\'' +
+                ", price='" + price + '\'' +
+                ", brand='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", category='" + category + '\'' +
+                '}';
+    }
 }
