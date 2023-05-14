8a0a71d36aea9f5cd3c84a81642d9dc89623a2f9
Artur Smith <arthur9smith7@gmail.com>
2023-05-14 14:40:07 +0200

    Implemented setting of products to the recycler. Added properties for recyclerView, productAdapter and viewModel.

--- app/src/main/java/com/example/foodi/MainActivity.java
+++ app/src/main/java/com/example/foodi/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.foodi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.List;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private ProductsAdapter productsAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+        MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        productsAdapter = new ProductsAdapter();
+        recyclerView.setAdapter(productsAdapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
 
+        viewModel.loadProducts();
+        viewModel.getProducts().observe(this, products -> productsAdapter.setProducts(products));
     }
 
     private void initViews() {
